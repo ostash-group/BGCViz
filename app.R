69a6ceec90dbe6cb04c41e5041f9cb77dfbb4e30
Pavlo Hrab <pavlo.hrab@lnu.edu.ua>
2021-10-09 10:34:27 +0300

    Fixed duplicates function now is fixing duplicates. This solved PRISM json upload bug

--- app.R
+++ app.R
@@ -243,13 +243,38 @@ server <- function(input, output, session) {
     is.integer(x) && length(x) == 0L
   }
   # Fix the duplicates in PRISM-Supp data. Therefore 1 row for 1 orf
-  fix_duplicates <-  function(test_score, order_vec, regul_genes_orfs){
+  fix_duplicates <-  function(test_score, order_vec, regul_genes_orfs, test_name){
     dupl_names <- regul_genes_orfs[duplicated(regul_genes_orfs)]
     duplicated_values <- which(duplicated(regul_genes_orfs[order_vec]))
     test_score <- test_score[order_vec]
     to_add <- test_score[(which(duplicated(regul_genes_orfs[order_vec])))]
     test_score <- test_score[-(which(duplicated(regul_genes_orfs[order_vec])))]
-    test_score[duplicated_values-1] <- paste(test_score[duplicated_values-1] , to_add, sep="/")
+    iterate_one_more_time <- c()
+    should_iterate = F
+    for (i in seq(1:length(test_name))){
+      if (test_name[i]==dupl_names[1]){
+        dupl_names = dupl_names[-1]
+        test_score[i] = paste0(test_score[i], "/" ,to_add[1])
+        to_add = to_add[-1]
+        iterate_one_more_time <- c(iterate_one_more_time, i)
+      }
+    }
+    if (length(iterate_one_more_time)>1){
+      should_iterate = T
+    }
+    while (should_iterate == T) {
+      for (i in iterate_one_more_time){
+        if (test_name[i] == dupl_names[1]){
+          dupl_names = dupl_names[-1]
+          test_score[i] = paste0(test_score[i], "/" ,to_add[1])
+          to_add = to_add[-1]
+        } 
+        if (length(dupl_names)==0){
+          should_iterate = F
+          break
+        }
+      }
+    }
     return(test_score)
   }
   # PRISM JSON data processing
@@ -299,21 +324,22 @@ server <- function(input, output, session) {
     reg_genes$Type2 <- reg_genes$Type
     
     test_name <- names[names %in% regul_genes_orfs]
+    ref_names <- test_name
     order_vec <- order(match(regul_genes_orfs, test_name))
     
     
     test_score <- sapply(data$prism_results$regulatory_genes, function(x){
       x$score
     })
-    reg_genes$Score  <- fix_duplicates(test_score , order_vec, regul_genes_orfs)
+    reg_genes$Score  <- fix_duplicates(test_score , order_vec, regul_genes_orfs, ref_names)
     test_name <- sapply(data$prism_results$regulatory_genes, function(x){
       x$name
     })
-    reg_genes$Name  <- fix_duplicates(test_name , order_vec, regul_genes_orfs)
+    reg_genes$Name  <- fix_duplicates(test_name , order_vec, regul_genes_orfs, ref_names)
     test_full_name<- sapply(data$prism_results$regulatory_genes, function(x){
       x$full_name
     })
-    reg_genes$Full_name  <- fix_duplicates(test_full_name , order_vec, regul_genes_orfs)
+    reg_genes$Full_name  <- fix_duplicates(test_full_name , order_vec, regul_genes_orfs, ref_names)
     resist_genes_orfs <- sapply(data$prism_results$resistance_genes, function(x){
       x$orf
     })
@@ -333,15 +359,15 @@ server <- function(input, output, session) {
     test_score <- sapply(data$prism_results$resistance_genes, function(x){
       x$score
     })
-    res_genes$Score  <- fix_duplicates(test_score , order_vec, resist_genes_orfs)
+    res_genes$Score  <- fix_duplicates(test_score , order_vec, resist_genes_orfs, ref_names)
     test_name <- sapply(data$prism_results$resistance_genes, function(x){
       x$name
     })
-    res_genes$Name  <- fix_duplicates(test_name , order_vec, resist_genes_orfs)
+    res_genes$Name  <- fix_duplicates(test_name , order_vec, resist_genes_orfs, ref_names)
     test_full_name<- sapply(data$prism_results$resistance_genes, function(x){
       x$full_name
     })
-    res_genes$Full_name  <- fix_duplicates(test_full_name , order_vec, resist_genes_orfs)
+    res_genes$Full_name  <- fix_duplicates(test_full_name , order_vec, resist_genes_orfs, ref_names)
     
     final_reg <- rbind(res_genes, reg_genes)
     final_reg$ID <- seq(1:dim(final_reg)[1])
