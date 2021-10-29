4f353b07be839c9e6dbffd8e2b8b5ef0eceb0020
Pavlo Hrab <pavlo.hrab@lnu.edu.ua>
2021-10-29 16:27:59 +0300

    Move validation functions to a separate file

--- app.R
+++ app.R
@@ -482,125 +482,7 @@ server <- function(input, output, session) {
   colnames(coloring_datatable) <- c("Name", "Color", "Hierarchy")
   vals$coloring_datatable <- DT::datatable(coloring_datatable,  rownames = F, editable = "column", options = list( dom='t',ordering=F))
   # Validation
-  check_if_column_exists <- function(data_names,column_name){
-    if (column_name %in% stringr::str_to_lower(data_names)){
-      return(TRUE)
-    } else {
-      shiny::showNotification( paste0(column_name, " column does not exist. Data was not integrated into analysis. Please recheck your data and try one more time"),type = "warning")
-      return(FALSE)
-    }
-  }
-  validate_basic_input <- function(data){
-    data_names <- names(data)
-    if (!(check_if_column_exists(data_names, 'cluster'))){
-      shiny::showNotification( paste0("Cluster columns was created on the fly."),type = "message")
-      data$Cluster <- seq(1:dim(data)[1])
-    }
-    if (!(check_if_column_exists(data_names, 'start'))){
-      return(FALSE)
-    }
-    if (!(check_if_column_exists(data_names, 'stop'))){
-      return(FALSE)
-    }
-    if (!(check_if_column_exists(data_names, 'type'))){
-      return(FALSE)
-    }
-    if (length(unique(data$Cluster)) != length(data$Cluster)){
-      shiny::showNotification( paste0("Cluster columns contains non unique values. It was regenerated"),type = "message")
-      data$Cluster <- seq(1:dim(data)[1])
-    }
-    if (( T %in% is.na(data$Start)) | (T %in% is.na(data$Stop))){
-      shiny::showNotification( paste0(" Start or Stop columns contain missing values. Please fix this and redownload dataframe"),type = "error")
-      return(FALSE)
-    }
-    if ((T %in% is.na(data$Type)) | ("" %in% data$Type)){
-      shiny::showNotification( paste0("Type column contain empty data. It was populated with 'unknown' "),type = "warning")
-      data$Type[is.na(data$Type)] <- 'unknown'
-      data$Type["" %in% data$Type] <- 'unknown'
-    }
-    if (!(is.numeric(data$Cluster))){
-      data$Cluster <- as.numeric(data$Cluster)
-    }
-    if (!(is.numeric(data$Start))){
-      data$Start <- as.numeric(data$Start)
-    }
-    if (!(is.numeric(data$Stop))){
-      data$Stop <- as.numeric(data$Stop)
-    }
-    if (!(is.character(data$Type))){
-      data$Type <- as.character(data$Type)
-    }
-    return(list(TRUE, data))
-  }
-  validate_rre_input <- function(data){
-    data_names <- names(data)
-    if (!(check_if_column_exists(data_names, 'gene.name'))){
-      return(FALSE)
-    }
-    if (F %in% grepl("__", data$Gene.name)){
-      return(FALSE)
-    }
-    if (!(check_if_column_exists(data_names, 'e.value'))){
-      return(FALSE)
-    } else {
-      data$E.value <- as.numeric(data$E.value)
-    }
-    if (!is.null(data$Probability)){
-      if (!(check_if_column_exists(data_names, 'score'))){
-        return(FALSE)
-      } else{
-        data$Score <- as.numeric(data$Score)
-      }
-      if (!(check_if_column_exists(data_names, 'p.value'))){
-        return(FALSE)
-      } else {
-        data$P.value <- as.numeric(data$P.value)
-      }
-      if (!(check_if_column_exists(data_names, 'rre.start'))){
-        return(FALSE)
-      } else {
-        data$RRE.start <- as.numeric(data$RRE.start)
-      }
-      if (!(check_if_column_exists(data_names, 'rre.end'))){
-        return(FALSE)
-      } else {
-        data$RRE.end <- as.numeric(data$RRE.end)
-      }
-      if (!(check_if_column_exists(data_names, 'probability'))){
-        return(FALSE)
-      } else {
-        data$Probability <- as.numeric(data$Probability)
-      }
-    }
-    return(list(TRUE, data))
-  }
-  validate_deep_input <- function(data){
-    data_names <- names(data)
-    col_names <- c("nucl_start", "nucl_end","num_proteins", "num_domains", "num_bio_domains","deepbgc_score","antibacterial",
-                   "cytotoxic","inhibitor","antifungal","alkaloid","nrp","other","polyketide","ripp","saccharide","terpene",
-                   "bgc_candidate_id", "sequence_id")
-    num_columns <- c("nucl_start", "nucl_end","num_proteins", "num_domains", "num_bio_domains","deepbgc_score","antibacterial",
-                     "cytotoxic","inhibitor","antifungal","alkaloid","nrp","other","polyketide","ripp","saccharide","terpene")
-    if (!('cluster' %in% stringr::str_to_lower(data_names))){
-      data$Cluster <- seq(1:dim(data)[1])
-    }
-    for (column_name in col_names){
-      if (!(check_if_column_exists(data_names, column_name))){
-        return(FALSE)
-      }
-      if ( T %in% is.na(data[[column_name]])){
-        return(FALSE)
-      }
-      if ( "" %in% data[[column_name]]){
-        return(FALSE)
-      }
-      if (column_name %in% num_columns){
-        names(data)[stringi::stri_trans_tolower(names(data)) == column_name] <- column_name
-        data[[column_name]] <- as.numeric(data[[column_name]])
-      }
-    }
-    return(list(TRUE, data))
-  }
+  source("src/validate_functions.R")
   # Variables, that holds data uploads boolean (so if data is present or not)
   data_uploads <- c("anti_data_input","sempi_data_input","prism_data_input","prism_supp_data_input",
                     "arts_data_input","deep_data_input","gecco_data_input","rre_data_input")
@@ -909,6 +791,7 @@ server <- function(input, output, session) {
   # TODO Make tidyr::separate functions for different data types. 
   # For now you just have duplicated the code. Specifically for ARTS!
   # Reading functions:
+  
   read_antismash <- function(data){
     anti_data <- data
     res_validation <- validate_basic_input(anti_data)
@@ -954,6 +837,7 @@ server <- function(input, output, session) {
   read_gecco <- function(data){
     # Add chromosome column
     gecco_data <- data
+    
     gecco_data$chromosome <-  rep("G", length(gecco_data$type))
     # Type magic
     gecco_data$Cluster <- seq(1:length(gecco_data$chromosome))
