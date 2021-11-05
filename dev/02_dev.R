63e0722143d8aa8fcee8d4d7d35748e1494c0dbb
Pavlo Hrab <pavlo.hrab@lnu.edu.ua>
2021-11-05 09:45:58 +0200

    Added deep_reference plot module
    Former-commit-id: a488a5c65f8392084ceb2d168c2d6723b3be1a94

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -40,8 +40,7 @@ usethis::use_package( "RSQLite" )
 
 ## Add modules ----
 ## Create a module infrastructure in R/
-golem::add_module( name = "name_of_module1" ) # Name of the module
-golem::add_module( name = "name_of_module2" ) # Name of the module
+golem::add_module( name = "deep_reference" ) # Name of the module
 
 ## Add helper functions ----
 ## Creates fct_* and utils_*
