d6a7412b9018af8dc2714112c45c6a0a0b8f56bd
Pavlo Hrab <pavlo.hrab@gmail.com>
2022-06-02 08:53:26 +0200

    Added antismash data

---
+++ data-raw/anti_data.R
@@ -0,0 +1,8 @@
+## code to prepare `anti_data` dataset goes here
+anti_data <- utils::read.csv("https://raw.githubusercontent.com/pavlohrab/BGCViz/raw_datasets/datasets/sco_antismash.csv")
+# Add chromosome column
+anti_data$chromosome <-  rep("A", length(anti_data$Cluster))
+# Type magic
+anti_data$Type <- stringr::str_trim(tolower(anti_data$Type))
+anti_data['Type2'] <- stringr::str_trim(tolower(anti_data$Type))
+usethis::use_data(anti_data, overwrite = TRUE)
