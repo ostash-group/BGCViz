bc0f2c18fc7315472de35d62302d99770be3324b
Tsarin2061 <tsarinlev@gmail.com>
2023-12-08 18:48:58 +0200

    working verstion

--- R/mod_group_table.R
+++ R/mod_group_table.R
@@ -89,9 +89,7 @@ mod_group_table_server <- function(id, vals, data_uploads, soft_names, soft_nami
                             g = paste(eval(as.name(excluded_names[7])), collapse = ","),
                             h = paste(eval(as.name(excluded_names[8])), collapse = ","),
                             i = paste(eval(as.name(excluded_names[9])), collapse = ","),
-                            j = paste(eval(as.name(excluded_names[10])), collapse = ","),
-
-                            
+                            j = paste(eval(as.name(excluded_names[10])), collapse = ",")
                         )
                     colnames(data) <- c(abbr[i], excluded_names)
                     for (p in abbr) {
